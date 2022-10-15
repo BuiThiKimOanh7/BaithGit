77c2959d7ea71f97cd5bb21990c01f392ec975bf
BuiThiKimOanh7 <2054052046Oanh@ou.edu.vn>
2022-10-15 15:02:45 +0700

    Init File

---
+++ baithGit/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baithGit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
+        }
+    }
+}
